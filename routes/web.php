f92d4c157f827820bcda7a24567e9308fe24711d
Ruturaj Subhedar <ruturajsubhedar1999@gmail.com>
2024-11-17 17:40:13 +0530

    db testing
    db testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/check-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection successful.';
+    } catch (\Exception $e) {
+        return 'Database connection failed: ' . $e->getMessage();
+    }
 });
